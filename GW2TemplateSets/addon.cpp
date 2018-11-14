78b974fb68cee6195d8d4f3741a78f9f0896e749
Cord Bleibaum <cordbleibaum@icloud.com>
2018-11-15 00:25:53 +0100

    Another bugfix

--- GW2TemplateSets/addon.cpp
+++ GW2TemplateSets/addon.cpp
@@ -60,6 +60,11 @@ arcdps_exports* mod_init()
 {
 	windowVisible = false;
 
+	if (!std::filesystem::exists("addons/templatesets/"))
+	{
+		std::filesystem::create_directory("addons/templatesets/");
+	}
+
 	rebuildSets();
 
 	setNameBufSize = 32;
